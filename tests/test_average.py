92fa16df8fc5805d36ef8406da08b24c96cea322
nanashi <egorglazkov71@gmail.com>
2024-06-24 20:20:55 +0300

    add test for factorial in hw 2

--- tests/test_average.py
+++ tests/test_average.py
@@ -2,29 +2,40 @@ import pytest
 from src.main_2 import average
 
 
-@pytest.mark.parametrize("value, expend_result",
+@pytest.mark.parametrize("value, expected_result",
                          [
                              ([1, 6, 4, 87, 34], 26.4),
                              ([-5, -1, -86, -5], -24.25),
-                             ([-5, -2, -65, 93, 2], 4.6)
+                             ([-5, -2, -65, 93, 2], 4.6),
+                             ([1.5, 5.2, 6.42, -7.6], 1.38),
+                             ([1, 5.3, -5, -6.32], -1.255)
                          ])
-def test_average_positive(value, expend_result):
-    assert average(value) == expend_result
+def test_average_positive(value, expected_result):
+    assert average(value) == expected_result
 
 
-@pytest.mark.parametrize("value, expend_result",
+@pytest.mark.parametrize("value, expected_result",
                          [
-                             ("str", TypeError())
+                             ("str", pytest.raises(TypeError))
                          ])
-def test_average_negative(value, expend_result):
-    assert average(value) == expend_result
+def test_average_negative(value, expected_result):
+    with expected_result:
+        assert average(value) == expected_result
 
 
-@pytest.mark.parametrize("value, expend_result",
+@pytest.mark.parametrize("value, expected_result",
                          [
-                             ([], ValueError()),
                              ([i for i in range(5000)], 2499.5),
                              ([68], 68)
                          ])
-def test_average_boundary(value, expend_result):
-    assert average(value) == expend_result
+def test_average_boundary_1(value, expected_result):
+    assert average(value) == expected_result
+
+
+@pytest.mark.parametrize("value, expected_result",
+                         [
+                             ([], pytest.raises(ValueError)),
+                         ])
+def test_average_boundary_2(value, expected_result):
+    with expected_result:
+        assert average(value) == expected_result
---
+++ tests/test_factorial.py
@@ -0,0 +1,32 @@
+import pytest
+from src.main_2 import factorial
+
+
+@pytest.mark.parametrize("value, expected_result",
+                         [
+                             (5.2, pytest.raises(TypeError)),
+                             ("5", pytest.raises(TypeError)),
+                             (-3, pytest.raises(ValueError)),
+                         ])
+def test_factorial_negative(value, expected_result):
+    with expected_result:
+        assert factorial(value) == expected_result
+
+
+@pytest.mark.parametrize("value, expected_result",
+                         [
+                             (10, 3628800)
+                         ])
+def test_factorial_positive(value, expected_result):
+    assert factorial(value) == expected_result
+
+
+@pytest.mark.parametrize("value, expected_result",
+                         [
+                             (0, 1),
+                             (50, 30414093201713378043612608166064768844377641568960512000000000000)
+                         ])
+def test_factorial_boundary(value, expected_result):
+    assert factorial(value) == expected_result
+
+
